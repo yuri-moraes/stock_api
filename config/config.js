205784269c0f09e69eaccb34ab83ac23ceecb54c
Yuri Nogueira de Moraes <yuri.nogueira.de.moraes@hotmail.com>
2024-08-26 11:32:22 -0300

    Adicionei os controllers e a config

---
+++ config/config.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+//DB CONFIG
+module.exports = {
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  define:{
+    timestamp: true
+  }
+};
+
