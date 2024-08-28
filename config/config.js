1b3b0be6cd40f8a20074b546805fa79400aa02b8
Yuri Nogueira de Moraes <yuri.nogueira.de.moraes@hotmail.com>
2024-08-28 16:14:07 -0300

    postgree config

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   dialect: "postgres",
-  url: process.env.DATABASE_URL,
+  url: process.env.POSTGRES_URL,
   define: {
     timestamps: true,
   },
--- config/sequelize.js
+++ config/sequelize.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
   dialect: "postgres",
   protocol: "postgres",
   dialectOptions: {
