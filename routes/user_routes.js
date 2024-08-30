6f80fbd371cab85ed4b91d00e76553ef5b1e4627
Yuri Moraes <yuri.nogueira.de.moraes@hotmail.com>
2024-08-30 10:19:55 -0300

    fix: spaces in user_routes.js

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -5,35 +5,14 @@ const protectedRoutesMiddleware = require("../middlewares/protectedRoutesMiddlew
 
 const userRouter = express.Router();
 
-userRouter.post(
-  "/register",
-  validationMiddleware.validateData,
-  UserController.register
-);
-userRouter.post(
-  "/login",
-  validationMiddleware.validateLogin,
-  UserController.login
-);
-// rotas protegidas
+// Rotas públicas
+userRouter.post("/register", validationMiddleware.validateData, UserController.register);
+userRouter.post("/login", validationMiddleware.validateLogin, UserController.login);
+
+// Rotas protegidas
 userRouter.get("/", protectedRoutesMiddleware, UserController.findAll);
-userRouter.get(
-  "/:id",
-  protectedRoutesMiddleware,
-  validationMiddleware.validadeUuid,
-  UserController.findById
-);
-userRouter.put(
-  "/edit/:id",
-  protectedRoutesMiddleware,
-  validationMiddleware.validadeUuid,
-  UserController.editUser
-);
-userRouter.delete(
-  "/:id",
-  protectedRoutesMiddleware,
-  validationMiddleware.validadeUuid,
-  UserController.delete
-);
+userRouter.get("/:id", protectedRoutesMiddleware, validationMiddleware.validadeUuid, UserController.findById);
+userRouter.put("/edit/:id", protectedRoutesMiddleware, validationMiddleware.validadeUuid, UserController.editUser);
+userRouter.delete("/:id", protectedRoutesMiddleware, validationMiddleware.validadeUuid, UserController.delete);
 
 module.exports = userRouter;
