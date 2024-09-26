139f84eeafa53223d437bbd520eede772f22c706
Marcos Vieira <marcosvieira90ss@gmail.com>
2024-09-26 15:35:26 -0300

    Coluna de userEmail inserida na tabela de logs

--- migrations/20240923192036-logs.js
+++ migrations/20240923192036-logs.js
@@ -24,6 +24,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false
       },
+      userEmail:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
       createdAt:{
         type: Sequelize.DATE
       },
