de3e179a84877aa0be17d8536e88e13dcb8381e8
Marcos Vieira <marcosvieira90ss@gmail.com>
2024-09-27 11:54:14 -0300

    deploy config

---
+++ config/config.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+module.exports = {
+  dialect: "postgres",
+  url: process.env.POSTGRES_URL,
+  define: {
+    timestamps: true,
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  dialectModule: require("pg"),
+};
---
+++ config/sequelize.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  dialectModule: require("pg"),
+});
+
+module.exports = sequelize;
